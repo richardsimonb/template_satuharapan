18f1a72cc1a48f54ddf2ccb8afdf81c1f7a4b9e7
richardsimonb <richardsimonb@gmail.com>
2020-02-17 14:08:42 +0700

    update facebook plugin

--- right.php
+++ right.php
@@ -176,14 +176,6 @@
     	?>
 	    <?=$link_other_smash?>
         </div>
-	
-	<div class="fb-page" 
-		data-href="https://www.facebook.com/satuharapan"
-		data-width="309" 
-		data-hide-cover="false"
-		data-show-facepile="false" 
-		data-show-posts="false">
-	    </div>
 <?php
     }
     ?>
@@ -207,3 +199,4 @@
     
 </div>           
 
+<div class="fb-page" data-href="https://www.facebook.com/satuharapan/" data-tabs="" data-width="" data-height="" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="false"><blockquote cite="https://www.facebook.com/satuharapan/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/satuharapan/">Satu Harapan</a></blockquote></div>
