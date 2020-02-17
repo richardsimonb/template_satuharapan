9a006ac49e45d440e0585d1ce4ba0a18f60baa0e
richardsimonb <richardsimonb@gmail.com>
2020-02-17 13:45:28 +0700

    edit facebook plugin

--- right.php
+++ right.php
@@ -80,13 +80,7 @@
             <img src="<?=baseURL.'/'.PATH_template?>mobile/images/BannerWismaSinarKasih309x220.jpg" />
         </a>
     </div>
-    <div class="fb-page" 
-        data-href="https://www.facebook.com/satuharapan"
-        data-width="309" 
-        data-hide-cover="false"
-        data-show-facepile="false" 
-        data-show-posts="false">
-    </div>
+    
     <!-- <div class=iklan_ataslife_rd><a href="http://www.satuharapan.com/uploads/tx_macinabanners/AdLarryBrook.jpg" target="_blank"><img src="http://www.satuharapan.com/uploads/tx_macinabanners/LarryBrook.jpg" /></a>
     </div>  -->
               
@@ -197,6 +191,14 @@
         </a>
     </div> -->
 
+    <div class="fb-page" 
+        data-href="https://www.facebook.com/satuharapan"
+        data-width="309" 
+        data-hide-cover="false"
+        data-show-facepile="false" 
+        data-show-posts="false">
+    </div>
+	
     <div class="isidaftarbaru">
         <!--<a href="http://danautoba.org/gcdt-2016/acara-kegiatan/" target="_blank">
         <img src="http://www.satuharapan.com/uploads/tx_macinabanners/Iklan_GCDT_II_2016_sm.jpg"  width="309">
