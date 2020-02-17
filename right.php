53956647da84ce4075d62285f1399ad93a4dde63
richardsimonb <richardsimonb@gmail.com>
2020-02-17 14:10:30 +0700

    Update right.php

--- right.php
+++ right.php
@@ -198,5 +198,3 @@
     </div>
     
 </div>           
-
-<div class="fb-page" data-href="https://www.facebook.com/satuharapan/" data-tabs="" data-width="" data-height="" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="false"><blockquote cite="https://www.facebook.com/satuharapan/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/satuharapan/">Satu Harapan</a></blockquote></div>
