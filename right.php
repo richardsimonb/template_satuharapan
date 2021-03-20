597f923c1237d027a718c2d3546795b306a29002
Richard Sengkey <richardsimonb@gmail.com>
2021-03-21 02:09:42 +0700

    change baseURL right

--- right.php
+++ right.php
@@ -8,14 +8,14 @@
 	        if($contentPhoto[0]['image']!=""){
     		    resizeFile($contentPhoto[0]['image'],309,206,"309x206_".$contentPhoto[0]['image']);
 	        ?>
-    		<span class="img-thumb"><a title="<?=$contentPhoto[0]['realtitle']?>" href="<?=baseURL.$link2.$contentPhoto[0]['value_alias']?>"><img src="<?=PATH_image_cache."309x206_".$contentPhoto[0]['image']?>" width="309" height="206" alt="<?=$contentPhoto[0]['realtitle']?>" /></a></span>
+    		<span class="img-thumb"><a title="<?=$contentPhoto[0]['realtitle']?>" href="<?='https://www.satuharapan.com'.$link2.$contentPhoto[0]['value_alias']?>"><img src="<?=PATH_image_cache."309x206_".$contentPhoto[0]['image']?>" width="309" height="206" alt="<?=$contentPhoto[0]['realtitle']?>" /></a></span>
             <?php
 	        }
 	        ?>
             <div class="titlelatest">
-			    <h4><a title="<?=$contentPhoto[0]['realtitle']?>" href="<?=baseURL.$link2.$contentPhoto[0]['value_alias']?>"><?=$contentPhoto[0]['title']?></a></h4>
+			    <h4><a title="<?=$contentPhoto[0]['realtitle']?>" href="<?='https://www.satuharapan.com'.$link2.$contentPhoto[0]['value_alias']?>"><?=$contentPhoto[0]['title']?></a></h4>
 			    <?=$contentPhoto[0]['preview']?>
-			    <a title="<?=$contentPhoto[0]['realtitle']?>" href="<?=baseURL.$link2.$contentPhoto[0]['value_alias']?>"><span class="more"></span></a>
+			    <a title="<?=$contentPhoto[0]['realtitle']?>" href="<?='https://www.satuharapan.com'.$link2.$contentPhoto[0]['value_alias']?>"><span class="more"></span></a>
 		    </div>
         </div>
         <div class="line"></div>
@@ -37,13 +37,13 @@
 			$class="titlenophoto";
 			if($image[0]!=""){
 			?>
-			<span class="img-thumb"><a title="<?=$contentPhoto[$m]['realtitle']?>" href="<?=baseURL.$link2.$contentPhoto[$m]['value_alias']?>"><img src="<?=PATH_image_cache."150x100_".$image[0]?>" width="150" height="100" alt="<?=$contentPhoto[$m]['realtitle']?>"/></a></span>
+			<span class="img-thumb"><a title="<?=$contentPhoto[$m]['realtitle']?>" href="<?='https://www.satuharapan.com'.$link2.$contentPhoto[$m]['value_alias']?>"><img src="<?=PATH_image_cache."150x100_".$image[0]?>" width="150" height="100" alt="<?=$contentPhoto[$m]['realtitle']?>"/></a></span>
 			<?php
 			$class="titlephoto";
 			}
 			?>
 			<div class="<?=$class?>">
-			    <a title="<?=$contentPhoto[$m]['realtitle']?>" href="<?=baseURL.$link2.$contentPhoto[$m]['value_alias']?>"><?=$contentPhoto[$m]['title']?></a>
+			    <a title="<?=$contentPhoto[$m]['realtitle']?>" href="<?='https://www.satuharapan.com'.$link2.$contentPhoto[$m]['value_alias']?>"><?=$contentPhoto[$m]['title']?></a>
 			</div>
 			<div class="clear"></div>
 		</div>
@@ -75,8 +75,8 @@
     </div>
 
     <div class=iklan_ataslife_rd>
-      <a href="<?=baseURL.'/'.PATH_template?>images/WismaSinarKasih0263_512609.jpg" target="_blank">
-          <img src="<?=baseURL.'/'.PATH_template?>mobile/images/BannerWismaSinarKasih309x220.jpg" />
+      <a href="<?='https://www.satuharapan.com'.'/'.PATH_template?>images/WismaSinarKasih0263_512609.jpg" target="_blank">
+          <img src="<?='https://www.satuharapan.com'.'/'.PATH_template?>mobile/images/BannerWismaSinarKasih309x220.jpg" />
       </a>
     </div>
     
@@ -135,14 +135,14 @@
 	        if($contentsmash[0]['image']!=""){
     			resizeFile($contentsmash[0]['image'],309,206,"309x206_".$contentsmash[0]['image']);
 	        ?>
-			    <span class="img-thumb"><a title="<?=$contentsmash[0]['realtitle']?>" href="<?=baseURL.$link2smash.$contentsmash[0]['value_alias']?>"><img src="<?=PATH_image_cache."309x206_".$contentsmash[0]['image']?>" width="309" height="206" alt="<?=$contentsmash[0]['realtitle']?>" /></a></span>
+			    <span class="img-thumb"><a title="<?=$contentsmash[0]['realtitle']?>" href="<?='https://www.satuharapan.com'.$link2smash.$contentsmash[0]['value_alias']?>"><img src="<?=PATH_image_cache."309x206_".$contentsmash[0]['image']?>" width="309" height="206" alt="<?=$contentsmash[0]['realtitle']?>" /></a></span>
 	        <?php
 		    }
 		    ?>
                 <div class="titlelatest">
-    			    <h4><a title="<?=$contentsmash[0]['realtitle']?>" href="<?=baseURL.$link2smash.$contentsmash[0]['value_alias']?>"><?=$contentsmash[0]['title']?></a></h4>
+    			    <h4><a title="<?=$contentsmash[0]['realtitle']?>" href="<?='https://www.satuharapan.com'.$link2smash.$contentsmash[0]['value_alias']?>"><?=$contentsmash[0]['title']?></a></h4>
 			        <?=$contentsmash[0]['preview']?>
-			        <a title="<?=$contentsmash[0]['realtitle']?>" href="<?=baseURL.$link2.$contentsmash[0]['value_alias']?>"><span class="more"></span></a>
+			        <a title="<?=$contentsmash[0]['realtitle']?>" href="<?='https://www.satuharapan.com'.$link2.$contentsmash[0]['value_alias']?>"><span class="more"></span></a>
 		        </div>
     	    </div>
 	        <div class="line"></div>
@@ -159,13 +159,13 @@
 			<?php
 			$class="titlenophoto";
 			if($image[0]!=""){?>
-			    <span class="img-thumb"><a title="<?=$contentsmash[$m]['realtitle']?>" href="<?=baseURL.$link2smash.$contentsmash[$m]['value_alias']?>"><img src="<?=PATH_image_cache."150x100_".$image[0]?>" width="150" height="100" alt="<?=$contentsmash[$m]['realtitle']?>"/></a></span>
+			    <span class="img-thumb"><a title="<?=$contentsmash[$m]['realtitle']?>" href="<?='https://www.satuharapan.com'.$link2smash.$contentsmash[$m]['value_alias']?>"><img src="<?=PATH_image_cache."150x100_".$image[0]?>" width="150" height="100" alt="<?=$contentsmash[$m]['realtitle']?>"/></a></span>
 			<?php
 			$class="titlephoto";
 			}
 			?>
 			    <div class="<?=$class?>">
-    				<a title="<?=$contentsmash[$m]['realtitle']?>" href="<?=baseURL.$link2smash.$contentsmash[$m]['value_alias']?>"><?=$contentsmash[$m]['title']?></a>
+    				<a title="<?=$contentsmash[$m]['realtitle']?>" href="<?='https://www.satuharapan.com'.$link2smash.$contentsmash[$m]['value_alias']?>"><?=$contentsmash[$m]['title']?></a>
 			    </div>
 			    <div class="clear"></div>
 		    </div>
