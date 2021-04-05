987c65f6ffc89902a30c7e63814754fabc50da3e
Richard Sengkey <richardsimonb@gmail.com>
2021-04-05 20:57:26 +0700

    edit right

--- right.php
+++ right.php
@@ -74,6 +74,11 @@
 	
     </div>
 
+	<div class=iklan_ataslife_rd>
+		<a href="<?='https://www.satuharapan.com'.'/'.PATH_template?>images/WismaSinarKasih0263_512609.jpg" target="_blank">
+			<img src="<?='https://www.satuharapan.com'.'/'.PATH_template?>mobile/images/BannerWismaSinarKasih309x220.jpg" />
+		</a>
+	</div>
     
     <!-- <div class=iklan_ataslife_rd><a href="http://www.satuharapan.com/uploads/tx_macinabanners/AdLarryBrook.jpg" target="_blank"><img src="http://www.satuharapan.com/uploads/tx_macinabanners/LarryBrook.jpg" /></a>
     </div>  -->
@@ -189,11 +194,6 @@
         <img src="http://www.satuharapan.com/uploads/tx_macinabanners/Iklan_GCDT_II_2016_sm.jpg"  width="309">
         </a>-->
     </div>
-    
+
 </div>           
 
-<div class=iklan_ataslife_rd>
-	<a href="<?='https://www.satuharapan.com'.'/'.PATH_template?>images/WismaSinarKasih0263_512609.jpg" target="_blank">
-		<img src="<?='https://www.satuharapan.com'.'/'.PATH_template?>mobile/images/BannerWismaSinarKasih309x220.jpg" />
-	</a>
-</div>
