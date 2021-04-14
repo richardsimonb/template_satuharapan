93e7a660ec8adcf871c5775915eb461ae4272584
Richard Sengkey <richardsimonb@gmail.com>
2021-04-14 23:48:40 +0700

    add control flow for category

--- subpages.php
+++ subpages.php
@@ -1,33 +1,80 @@
 <?php
-require "header.php";
+if($namepage=="category.php"){
 ?>
-<div id="content">
-    <!--- Content Top --->
-    <div id="ct-top" class="homepage">
-		<div id="ctop-left">
+	<?php
+	require "header.php";
+	?>
+
+	<div id="content">
+		<!--- Content Top --->
+		<div id="ct-top" class="homepage">
+			<div id="ctop-left">
+				<?php
+				require $namepage;
+				?>
+			</div>
 			<?php
-			require $namepage;
+			require "right.php";
 			?>
+			<div class="clear line"></div>
 		</div>
-		<?php
-		require "right.php";
-		?>
-        <div class="clear line"></div>
-    </div>
-	<div id="ct-cent">
-		<?php
-		require "latestnews.php";
-		require "opini.php";
-		require "popular.php";
-		?>
-		<div id="footermenu">
-			<ul>
-			<?=$bottommenu?>
-			</ul>
+		<div id="ct-cent">
+			<?php
+			require "latestnews.php";
+			require "opini.php";
+			require "popular.php";
+			?>
+			<div id="footermenu">
+				<ul>
+				<?=$bottommenu?>
+				</ul>
+			</div>
 		</div>
 	</div>
-</div>
 
 	<?php
 	require "footer.php";
 	?>
+
+<?php
+} else {
+?>
+
+	<?php
+	require "header.php";
+	?>
+
+	<div id="content">
+		<!--- Content Top --->
+		<div id="ct-top" class="homepage">
+			<div id="ctop-left">
+				<?php
+				require $namepage;
+				?>
+			</div>
+			<?php
+			require "right.php";
+			?>
+			<div class="clear line"></div>
+		</div>
+		<div id="ct-cent">
+			<?php
+			require "latestnews.php";
+			require "opini.php";
+			require "popular.php";
+			?>
+			<div id="footermenu">
+				<ul>
+				<?=$bottommenu?>
+				</ul>
+			</div>
+		</div>
+	</div>
+
+	<?php
+	require "footer.php";
+	?>
+<?php
+}
+?>
+
