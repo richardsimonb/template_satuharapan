764fc55eca9c3bc238af81946e269c50c13d1220
Richard Sengkey <richardsimonb@gmail.com>
2021-04-26 14:57:48 +0700

    hide banner

--- right.php
+++ right.php
@@ -115,11 +115,11 @@
 <?php
     }
     ?>
-<div class=iklan_ataslife_rd>
-		<a href="<?='https://www.satuharapan.com'.'/'.PATH_template?>images/WismaSinarKasih0263_512609.jpg" target="_blank">
-			<img src="<?='https://www.satuharapan.com'.'/'.PATH_template?>mobile/images/BannerWismaSinarKasih309x220.jpg" />
-		</a>
-	</div>
+<div class="iklan_ataslife_rd none">
+	<a href="<?='https://www.satuharapan.com'.'/'.PATH_template?>images/WismaSinarKasih0263_512609.jpg" target="_blank">
+		<img src="<?='https://www.satuharapan.com'.'/'.PATH_template?>mobile/images/BannerWismaSinarKasih309x220.jpg" />
+	</a>
+</div>
 	
 </div>           
 
