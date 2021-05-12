5c70698946c771c7c1093778e3a897ee91664b04
Richard Sengkey <richardsimonb@gmail.com>
2021-05-12 19:12:00 +0700

    edit banner mobile

--- mobile/index.php
+++ mobile/index.php
@@ -148,7 +148,7 @@ require "header.php";
 
 		<!-- Tempat Banner full5 -->
 		<div class="share">
-            <div><a href="<?='https://www.satuharapan.com'.'/'.PATH_template?>/images/WismaSinarKasih0263_512609.jpg" target="_blank"><img src="<?='https://www.satuharapan.com'.'/'.PATH_template?>mobile/images/BannerWismaSinarKasih309x220.jpg" /></a></div>
+            <div class="none"><a href="<?='https://www.satuharapan.com'.'/'.PATH_template?>/images/WismaSinarKasih0263_512609.jpg" target="_blank"><img src="<?='https://www.satuharapan.com'.'/'.PATH_template?>mobile/images/BannerWismaSinarKasih309x220.jpg" /></a></div>
 			<img src="/fileadmin/banners/kenaikan21.jpg"/>
 		</div>
 
